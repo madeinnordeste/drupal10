8c81ce15a8f654bc1c65a81ca6a9c92ff194cf7f
Luiz Alberto <madeinnordeste@gmail.com>
2023-10-06 17:49:19 -0300

    start

---
+++ config/env/settings.local.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal10',
+  'username' => 'drupal10',
+  'password' => 'drupal10',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '',
+  'isolation_level' => 'READ COMMITTED',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/development.services.yml';
